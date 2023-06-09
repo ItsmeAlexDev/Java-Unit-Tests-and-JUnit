cb26266a04c57b8c51b56ed1c3c92d848d9dc780
ItsmeAlexDev <77814458+ItsmeAlexDev@users.noreply.github.com>
2023-06-08 23:11:01 -0300

    Add a simple class made with TDD
    Simple test to try and learn how to actually develop with TDD (Test-driven development), which means the tests were written first.

---
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -0,0 +1,20 @@
+package br.ce.wcaquino.servicos;
+
+public class Calculadora {
+	
+	public int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtrai(int a, int b) {
+		return a - b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
+	
+	public int multiplica(int a, int b) {
+		return a * b;
+	}
+}
---
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -0,0 +1,71 @@
+package br.ce.wcaquino.servicos;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculadoraTest{
+	
+	private int a;
+	private int b;
+	private Calculadora calc;
+	private int resultado;
+	
+	@Before
+	public void setup() {
+		a = 0;
+		b = 0;
+		calc = new Calculadora();
+		resultado = 0;
+	}
+	
+	@Test
+	public void somaDoisValores() {
+		a = 3;
+		b = 5;
+		
+		resultado = calc.soma(a, b);
+		
+		assertEquals(8, resultado);
+	}
+	
+	@Test
+	public void subtraiDoisValores() {
+		a = 8;
+		b = 5;
+		
+		resultado = calc.subtrai(a, b);
+				
+		assertEquals(3, resultado);
+	}
+	
+	@Test
+	public void divideDoisValores() {
+		a = 6;
+		b = 3;
+		
+		resultado = calc.divide(a, b);
+		
+		assertEquals(2, resultado);
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void exceptionDivisaoPorZero() {
+		a = 10;
+		b = 0;
+		
+		calc.divide(a, b);
+	}
+	
+	@Test
+	public void multiplicaDoisValores() {
+		a = 6;
+		b = 3;
+		
+		resultado = calc.multiplica(a, b);
+		
+		assertEquals(18, resultado);
+	}
+	
+}
